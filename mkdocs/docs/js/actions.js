58ab55258b8341009222be9feb997875f360f125
Hassan Radi <hassan.muhammad1990@gmail.com>
2023-12-29 21:11:16 +0100

    [Bug fix] fix a small bug that prevented the pipeline message from showing

--- mkdocs/docs/js/actions.js
+++ mkdocs/docs/js/actions.js
@@ -479,7 +479,7 @@ class Actions {
         // TODO: find a way to tie the MR with the pipeline
         let intervalID = window.setInterval(async () => {
           await this.getPipelineUrl().then(pipelineUrl => {
-            if(pipelineUrl === undefined){
+            if(pipelineUrl === null || pipelineUrl === undefined || pipelineUrl.isEmpty()){
               this.showModalMessage(true, "Your prompt is getting created!", true,
                   "<br>Waiting for pipeline...");
             } else {
